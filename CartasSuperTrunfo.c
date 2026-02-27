c28df7692bf35bae73cbfb6e47a55621c8ccee9b
klvinfrtas <freitaskelvin14@gmail.com>
2026-02-27 20:25:38 +0000

    Fiz as variáveis das duas cartas.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,9 +6,27 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+    //Informações das variáveis de carta 1.
+  char estado1;
+  char codigodacarta1[4];
+  char nomedacidade1 [20];
+  int populacao1;
+  float area1;
+  float pib1;
+  int numdepontosturisticos1; //"num" de número.
+
+    //Informações das variáveis da carta 2.
+  char estado2;
+  char codigodacarta2[4];
+  char nomedacidade2[20];
+  int populacao2;
+  float area2;
+  float pib2;
+  int numdepontosturiscos2; //igual ao "num" acima.
 
   // Área para entrada de dados
-
+    //Vou usar "fgets" para que as pessoas tenham mais liberdade de escolha.
+    
   // Área para exibição dos dados da cidade
 
 return 0;
