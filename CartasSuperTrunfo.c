38285207ff96bf176429a5e9fbfb18a7c938f3d1
klvinfrtas <freitaskelvin14@gmail.com>
2026-03-07 19:33:32 +0000

    Toda a implementação das estruturas de decisão. além de alguns códigos para remover o \n do fgets

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,8 +2,7 @@
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
-
+// Objetivo: Nível mestre.
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
     //Informações das variáveis de carta 1.
@@ -16,6 +15,7 @@ int main() {
   int numdepontosturisticos1;
   float densidadepopulacional1;
   float pibpercapita1;
+  float superpoder1;
 
     //Informações das variáveis da carta 2.
   char estado2[3];
@@ -27,6 +27,7 @@ int main() {
   int numdepontosturisticos2;
   float densidadepopulacional2;
   float pibpercapita2;
+  float superpoder2;
 
       //<<ATENÇÃO>> Duas informações "úteis" sobre o código acima.    
           //Após uma pesquisa rápida, o nome mais longe de uma cidade são 32 caracteres, só arredondei para 35 para ficar melhor.
@@ -58,7 +59,7 @@ int main() {
 
   printf("Digite o número de pontos turísticos da cidade: ");
     scanf("%i", &numdepontosturisticos1);
-      getchar();
+      getchar(); //limpa o buffer para poder dar sequência a segunda carta.
 
     densidadepopulacional1 = (float) populacao1 / area1;
     pibpercapita1 = area1 / pib1; 
@@ -72,7 +73,6 @@ int main() {
 
   printf("Vamos escolher uma única letra de um estado (de A até o H): ");
     fgets(estado2, 3, stdin);
-      //printf("sua letra é: %c\n", estado2); Isso aqui foi usado para teste, para ver se a letra estava sendo armazenada corretamente.
         
   printf("Agora, a letra do estado que escolheu junto com dois números de 1 a 4 (ex: A01, B02, etc): ");
     fgets(codigodacarta2, 5, stdin);
@@ -121,5 +121,70 @@ int main() {
   printf("Densidade Populacional: %.2f hab/km²\n", densidadepopulacional2);
   printf("PIB per capita: R$ %.2f milhões\n", pibpercapita2);
 
+  //Estruturas de decisão das cartas
+
+  //Essas linhas serão para arrumar o \n que existe no fgets
+  //Também explica a nova livraria que coloquei
+    nomedacidade1[strcspn(nomedacidade1, "\n")] = 0;
+    nomedacidade2[strcspn(nomedacidade2, "\n")] = 0; 
+
+  printf("\nQuem ganhou?? Vamos comparar cada atributo!\n");
+
+ printf("Atributo: Populacão\n");
+        printf("\nCarta 1 - %s: %d\n", nomedacidade1, populacao1);
+        printf("Carta 2 - %s: %d\n\n", nomedacidade2, populacao2);
+            if(populacao1 > populacao2){
+              printf("%s ganhou!!!\n\n", nomedacidade1);
+            } else {
+              printf("%s ganhou!!!\n\n", nomedacidade2);
+            }
+
+  printf("\nAtributo: Area\n");
+        printf("\nCarta 1 - %s: %.2f hab/km²\n", nomedacidade1, area1);
+        printf("Carta 2 - %s: %.2f hab/km²\n\n", nomedacidade2, area2);
+            if(area1 > area2){
+              printf("%s ganhou!!!\n\n", nomedacidade1);
+            } else {
+              printf("%s ganhou!!!\n\n", nomedacidade2);
+            }
+
+  printf("\nAtributo: PIB\n"
+            "\nCarta 1 - %s: R$ %.2f Milhões\n", nomedacidade1, pib1);
+        printf("Carta 2 - %s: R$ %.2f Milhões\n\n", nomedacidade2, pib2);
+            if(pib1 > pib2){
+              printf("%s ganhou!!!\n\n", nomedacidade1);
+            } else {
+              printf("%s ganhou!!!\n\n", nomedacidade2);
+            }
+
+  printf("\nAtributo: Pontos Turísticos\n"
+            "\nCarta 1 - %s: %d\n", nomedacidade1, numdepontosturisticos1);
+        printf("Carta 2 - %s: %d\n\n", nomedacidade2, numdepontosturisticos2);
+            if(numdepontosturisticos1 > numdepontosturisticos2){
+              printf("%s ganhou!!!\n\n", nomedacidade1);
+            } else {
+              printf("%s ganhou!!!\n\n", nomedacidade2);
+            }
+
+  printf("\nAtributo: PIB per capita\n"
+            "\nCarta 1 - %s: R$ %.2f Mil\n", nomedacidade1, pibpercapita1);
+        printf("Carta 2 - %s: R$ %.2f Mil\n\n", nomedacidade2, pibpercapita2);
+            if(populacao1 > populacao2){
+              printf("%s ganhou!!!\n\n", nomedacidade1);
+            } else {
+              printf("%s ganhou!!!\n\n", nomedacidade2);
+            }
+
+  printf("\nAtributo: Densidade Populacional\n"
+            "\nCarta 1 - %s: %.2f hab/km²\n", nomedacidade1, densidadepopulacional1);
+        printf("Carta 2 - %s: %.2f hab/km²\n\n", nomedacidade2, densidadepopulacional2);
+            if(populacao1 < populacao2){
+              printf("%s ganhou!!!\n", nomedacidade1);
+            } else {
+              printf("%s ganhou!!!\n", nomedacidade2);
+            }
+  
+  printf("\nEm densidade populacional, é feito o inverso, quem tiver a menor densidade, ganha.\n");
+  
 return 0;
 } 
